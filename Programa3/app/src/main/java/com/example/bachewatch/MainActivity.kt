115b3a64397c32f5c626b8bb396c91801991a111
melanie105 <melaniersm105@gmail.com>
2026-06-13 22:57:55 -0600

    cambios

--- Programa3/app/src/main/java/com/example/bachewatch/MainActivity.kt
+++ Programa3/app/src/main/java/com/example/bachewatch/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.bachewatch.ui.navigation.BacheWatchNavGraph
 import com.example.bachewatch.ui.theme.BacheWatchTheme
+import com.google.android.gms.maps.MapsInitializer
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class  MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MapsInitializer.initialize(applicationContext)
         enableEdgeToEdge()
         setContent {
             BacheWatchTheme {
